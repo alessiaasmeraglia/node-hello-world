5305f62e44cd87b931eb9f8d1f1c51cbc461d4dd
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-22 15:21:33 +0200

    Aggiunto super bonus con libreria cowsayjs

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 //console.log("Hello World");
 //console.log("Hello Boolean");
-const args = process.argv.slice(2);
+/* const args = process.argv.slice(2);
 
 const message = args.join(" ");
 
-console.log(message || "Hello Boolean");
+console.log(message || "Hello Boolean"); */
+
+import { cowsay, cowthink, moo } from "cowsayjs";
+const phrases = [
+  "Hello Boolean",
+  "Sto imparando Node.js",
+  "Node gira fuori dal browser",
+  "Le mucche sanno programmare",
+  "Perché JavaScript non va mai al mare? Perché teme le onde asincrone!"
+];
+
+const args = process.argv.slice(2);
+
+const terminalMessage = args.join(" ");
+
+const randomIndex = Math.floor(Math.random() * phrases.length);
+
+const randomPhrase = phrases[randomIndex];
+
+const finalMessage = terminalMessage || randomPhrase;
+
+console.log(cowsay(finalMessage));
