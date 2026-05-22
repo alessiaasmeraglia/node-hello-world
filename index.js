c77f0d572e0746c2285194b78bccb43c8535b5a6
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-22 15:14:50 +0200

    Aggiunto bonus con process.argv

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 //console.log("Hello World");
-console.log("Hello Boolean");
+//console.log("Hello Boolean");
+const args = process.argv.slice(2);
+
+const message = args.join(" ");
+
+console.log(message || "Hello Boolean");
